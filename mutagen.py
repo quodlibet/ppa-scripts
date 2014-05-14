c5672aa1bc2aa6d6dcb960e90746aeed3c6f1e22
Christoph Reiter <reiter.christoph@gmail.com>
2014-05-15 01:35:28 +0200

    remove quodlibet-plugins packaging

--- mutagen.py
+++ mutagen.py
@@ -7,7 +7,7 @@ from _util import *
 
 PACKAGE= "mutagen"
 RELEASE_TAG = "mutagen-1.22"
-PPA_VERSION = "1.22.99"
+PPA_VERSION = "1.23.99"
 RELEASE_VERSION = "1.22"
 
 ##########################################################
