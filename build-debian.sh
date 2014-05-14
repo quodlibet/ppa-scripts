e4230a855b4cd5a4f86ff2ff92c71e2397ddc422
Christoph Reiter <reiter.christoph@gmail.com>
2014-05-15 01:35:00 +0200

    update debian packaging for plugin merge

--- build-debian.sh
+++ build-debian.sh
@@ -5,7 +5,6 @@ mini-dinstall -k
 rm -rf ~/debian_archive/quodlibet*
 mini-dinstall
 ./quodlibet.py -ddebian
-./quodlibet-plugins.py -ddebian
 ./mutagen.py -ddebian
 mini-dinstall -r
 cd ~/debian_archive/quodlibet-unstable/
