e2bedd970304a0246ce7a9a8255ef79afc106eff
Christoph Reiter <reiter.christoph@gmail.com>
2017-01-01 13:49:10 +0100

    copr repo src move

--- copr/push.sh
+++ copr/push.sh
@@ -19,7 +19,7 @@ cd workingdir
 git init
 git add .
 git commit -m "update"
-git remote add origin https://github.com/lazka/ql-copr.git
+git remote add origin https://github.com/quodlibet/ql-copr.git
 git push --force --set-upstream origin master
 cd ..
 
@@ -27,7 +27,7 @@ cd ..
 srpm_path=$(find -name "*src.rpm")
 bname=$(basename $srpm_path)
 
-./new_build.py "https://github.com/lazka/ql-copr/raw/master/SRPMS/$bname"
+./new_build.py "https://github.com/quodlibet/ql-copr/raw/master/SRPMS/$bname"
 
 # clean up
 rm -Rf workingdir
